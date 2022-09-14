fbf8c9b24506bc1a81bb4a60fa67fb5dd48b679f
PedroMesquitaIsidoro <83170114+PedroMesquitaIsidoro@users.noreply.github.com>
2022-09-14 13:46:45 -0300

    sim
    sim

---
+++ Gerenciador_de_Projetoss/src/entities/Usuario.java
@@ -0,0 +1,71 @@
+package entities;
+
+public class Usuario {
+	
+	private String email;
+	private String senha;
+	private String nome;
+	private String funcao;
+	boolean ehCoordenador;
+	Projeto ehVinculadoProjeto;
+	
+	public Usuario() {
+		
+	}
+	
+	public Usuario(String email, String senha, String nome, String funcao) {
+		this.email = email;
+		this.senha = senha;
+		this.nome = nome;
+		this.funcao = funcao;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getFuncao() {
+		return funcao;
+	}
+
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+	
+	public boolean EhCoordenador() {
+		return ehCoordenador;
+	}
+	
+	public void setEhCoordenador(boolean ehCoordenador) {
+		this.ehCoordenador = ehCoordenador;
+	}
+	
+	public Projeto EhVinculado() {
+		return ehVinculadoProjeto;
+	}
+	
+	public void setEhVinculador(boolean ehVinculadoPorjeto) {
+		this.ehVinculadoProjeto = ehVinculadoProjeto;
+	}
+	
+}
